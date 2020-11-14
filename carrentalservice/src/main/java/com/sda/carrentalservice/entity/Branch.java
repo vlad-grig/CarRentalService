519edf1f816785da8358dc0ef24e40036ad0907d
George Niculae <george.niculae20@yahoo.com>
2020-11-14 12:02:49 +0200

    Entities without connections

--- carrentalservice/src/main/java/com/sda/carrentalservice/entity/Branch.java
+++ carrentalservice/src/main/java/com/sda/carrentalservice/entity/Branch.java
@@ -1,9 +1,16 @@
 package com.sda.carrentalservice.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.List;
 
+@Entity
 public class Branch {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String address;
     private List<Employee> employees;
