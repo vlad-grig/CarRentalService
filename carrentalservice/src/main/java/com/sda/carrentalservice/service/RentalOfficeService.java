35ed6c24073f2a9524716a286c951aaddfeb07ff
George Niculae <george.niculae20@yahoo.com>
2020-11-15 11:17:48 +0200

    no message

--- carrentalservice/src/main/java/com/sda/carrentalservice/service/RentalOfficeService.java
+++ carrentalservice/src/main/java/com/sda/carrentalservice/service/RentalOfficeService.java
@@ -1,8 +1,12 @@
 package com.sda.carrentalservice.service;
 
+import com.sda.carrentalservice.entity.RentalOffice;
 import com.sda.carrentalservice.repository.RentalOfficeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class RentalOfficeService {
@@ -12,5 +16,27 @@ public class RentalOfficeService {
     @Autowired
     public RentalOfficeService(RentalOfficeRepository rentalOfficeRepository) {
         this.rentalOfficeRepository = rentalOfficeRepository;
+    }
+
+    public RentalOffice saveRentalOffice(RentalOffice rentalOffice) {
+        return rentalOfficeRepository.save(rentalOffice);
+    }
+
+    public List<RentalOffice> findAllRentalOffices() {
+        return rentalOfficeRepository.findAll();
+    }
+
+    public void deleteRentalOfficeById(Long id) {
+        rentalOfficeRepository.deleteById(id);
+    }
+
+    public RentalOffice findRentalOfficeById(Long id) {
+        Optional<RentalOffice> optionalRentalOffice = rentalOfficeRepository.findById(id);
+        if (optionalRentalOffice.isPresent()) {
+            RentalOffice rentalOffice = optionalRentalOffice.get();
+            return rentalOffice;
+        } else {
+            throw new RuntimeException();
+        }
     }
 }
