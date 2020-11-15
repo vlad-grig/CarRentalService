c22e29c87ff6de7929ff48bf99d8a92428aaf329
George Niculae <george.niculae20@yahoo.com>
2020-11-15 13:22:43 +0200

    no message

--- carrentalservice/src/main/java/com/sda/carrentalservice/mvccontroller/EmployeeMVCController.java
+++ carrentalservice/src/main/java/com/sda/carrentalservice/mvccontroller/EmployeeMVCController.java
@@ -1,7 +1,16 @@
 package com.sda.carrentalservice.mvccontroller;
 
+import com.sda.carrentalservice.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class EmployeeMVCController {
+
+    private final EmployeeService employeeService;
+
+    @Autowired
+    public EmployeeMVCController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
 }
