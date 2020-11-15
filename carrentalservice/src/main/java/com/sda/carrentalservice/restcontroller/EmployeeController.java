d481c5cbf48f3ccd51edffa3138477f16a7ca839
George Niculae <george.niculae20@yahoo.com>
2020-11-15 11:42:16 +0200

    no message

--- carrentalservice/src/main/java/com/sda/carrentalservice/restcontroller/EmployeeController.java
+++ carrentalservice/src/main/java/com/sda/carrentalservice/restcontroller/EmployeeController.java
@@ -1,7 +1,21 @@
 package com.sda.carrentalservice.restcontroller;
 
+import com.sda.carrentalservice.service.EmployeeService;
+import com.sda.carrentalservice.transformer.EmployeeTransformer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmployeeController {
+
+    private final EmployeeService employeeService;
+    private final EmployeeTransformer employeeTransformer;
+
+    @Autowired
+    public EmployeeController(EmployeeService employeeService, EmployeeTransformer employeeTransformer) {
+        this.employeeService = employeeService;
+        this.employeeTransformer = employeeTransformer;
+    }
+
+    
 }
