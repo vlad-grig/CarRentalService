ba9a342399eb6ff4d50f3477e600b0ffc753825f
George Niculae <george.niculae20@yahoo.com>
2020-11-15 10:46:39 +0200

    no message

--- carrentalservice/src/main/java/com/sda/carrentalservice/service/EmployeeService.java
+++ carrentalservice/src/main/java/com/sda/carrentalservice/service/EmployeeService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmployeeService {
@@ -25,7 +26,25 @@ public class EmployeeService {
         employeeRepository.save(employee);
     }
 
-//    public List<Employee> getAllEmployees() {
-//
-//    }
+    public Employee saveEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public List<Employee> findAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    public void deleteEmployeeById(Long id) {
+        employeeRepository.deleteById(id);
+    }
+
+    public Employee findEmployeeById(Long id) {
+        Optional<Employee> optionalEmployee = employeeRepository.findById(id);
+        if (optionalEmployee.isPresent()) {
+            Employee employee = optionalEmployee.get();
+            return employee;
+        } else {
+            throw new RuntimeException();
+        }
+    }
 }
