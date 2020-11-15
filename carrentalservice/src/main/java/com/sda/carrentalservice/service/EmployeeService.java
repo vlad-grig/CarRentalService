c92638d1c66f16ed35d360c7c7bad93588c13b04
George Niculae <george.niculae20@yahoo.com>
2020-11-15 10:23:35 +0200

    no message

--- carrentalservice/src/main/java/com/sda/carrentalservice/service/EmployeeService.java
+++ carrentalservice/src/main/java/com/sda/carrentalservice/service/EmployeeService.java
@@ -1,8 +1,12 @@
 package com.sda.carrentalservice.service;
 
+import com.sda.carrentalservice.dto.EmployeeDTO;
+import com.sda.carrentalservice.entity.Employee;
 import com.sda.carrentalservice.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class EmployeeService {
@@ -13,4 +17,15 @@ public class EmployeeService {
     public EmployeeService(EmployeeRepository employeeRepository) {
         this.employeeRepository = employeeRepository;
     }
+
+    public void addEmployee(EmployeeDTO employeeDTO) {
+        Employee employee = new Employee();
+//        employee.setFirstName();
+//        employee.setLastName();
+        employeeRepository.save(employee);
+    }
+
+//    public List<Employee> getAllEmployees() {
+//
+//    }
 }
---
+++ carrentalservice/src/main/java/com/sda/carrentalservice/transformer/RentalOfficeTransformer.java
@@ -0,0 +1,4 @@
+package com.sda.carrentalservice.transformer;
+
+public class RentalOfficeTransformer {
+}
