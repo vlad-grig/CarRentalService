4cc5737b462406e23ffc7e7b5e5fa168bdc11132
George Niculae <george.niculae20@yahoo.com>
2020-11-16 08:57:18 +0200

    no message

--- carrentalservice/src/main/java/com/sda/carrentalservice/mvccontroller/EmployeeMVCController.java
+++ carrentalservice/src/main/java/com/sda/carrentalservice/mvccontroller/EmployeeMVCController.java
@@ -1,11 +1,17 @@
 package com.sda.carrentalservice.mvccontroller;
 
+import com.sda.carrentalservice.entity.Employee;
 import com.sda.carrentalservice.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class EmployeeMVCController {
@@ -27,5 +33,25 @@ public class EmployeeMVCController {
     public String deleteEmployeeById(@PathVariable("id") Long id) {
         employeeService.deleteEmployeeById(id);
         return "redirect:/";
+    }
+
+    @PostMapping(path = "/employee/add")
+    public String addEmployee(@ModelAttribute("employee") @Valid Employee employee, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "add-employee";
+        } else {
+            this.employeeService.saveEmployee(employee);
+            return "redirect:/";
+        }
+    }
+
+    @PostMapping(path = "/employee/update")
+    public String editEmployee(@ModelAttribute("employee") @Valid Employee employee, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "edit-employee";
+        } else {
+            this.employeeService.saveEmployee(employee);
+            return "redirect:/";
+        }
     }
 }
