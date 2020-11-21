c955e8ef17d17547c0bc9168e9d217d083055196
Ovidiu Piorescu <djenergydj@yahoo.com>
2020-11-21 13:41:54 +0200

    no message

--- carrentalservice/src/main/java/com/sda/carrentalservice/entity/Revenue.java
+++ carrentalservice/src/main/java/com/sda/carrentalservice/entity/Revenue.java
@@ -9,12 +9,20 @@ import javax.persistence.Id;
 public class Revenue extends BaseEntity{
 
     private Double sumOfAmountsForCarRental;
-
-    public Revenue(Double sumOfAmountsForCarRental) {
-        this.sumOfAmountsForCarRental = sumOfAmountsForCarRental;
-    }
+    private Double dailyRevenue;
+    private Double weeklyRevenue;
+    private Double monthlyRevenue;
+    private Double yearlyRevenue;
 
     public Revenue() {
+    }
+
+    public Revenue(Double sumOfAmountsForCarRental, Double dailyRevenue, Double weeklyRevenue, Double monthlyRevenue, Double yearlyRevenue) {
+        this.sumOfAmountsForCarRental = sumOfAmountsForCarRental;
+        this.dailyRevenue = dailyRevenue;
+        this.weeklyRevenue = weeklyRevenue;
+        this.monthlyRevenue = monthlyRevenue;
+        this.yearlyRevenue = yearlyRevenue;
     }
 
     public Double getSumOfAmountsForCarRental() {
@@ -23,5 +31,21 @@ public class Revenue extends BaseEntity{
 
     public void setSumOfAmountsForCarRental(Double sumOfAmountsForCarRental) {
         this.sumOfAmountsForCarRental = sumOfAmountsForCarRental;
+    }
+
+    public void getDailyRevenue(Double dailyRevenue) {
+        this.dailyRevenue = dailyRevenue;
+    }
+
+    public void getWeeklyRevenue(Double weeklyRevenue) {
+        this.weeklyRevenue = weeklyRevenue;
+    }
+
+    public void getMonthlyRevenue(Double monthlyRevenue) {
+        this.monthlyRevenue = monthlyRevenue;
+    }
+
+    public void getYearlyRevenue(Double yearlyRevenue) {
+        this.yearlyRevenue = yearlyRevenue;
     }
 }
--- carrentalservice/src/main/java/com/sda/carrentalservice/service/RentalService.java
+++ carrentalservice/src/main/java/com/sda/carrentalservice/service/RentalService.java
@@ -1,11 +1,13 @@
 package com.sda.carrentalservice.service;
 
 import com.sda.carrentalservice.entity.Rental;
+import com.sda.carrentalservice.entity.RentalOffice;
 import com.sda.carrentalservice.repository.RentalRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class RentalService {
@@ -24,4 +26,18 @@ public class RentalService {
     public List<Rental> findAllRentals() { return rentalRepository.findAll(); }
 
     public void deleteAllRentals() { rentalRepository.deleteAll(); }
+
+    public void deleteRentalOfficeById(Long id) {
+        rentalRepository.deleteById(id);
+    }
+
+    public Rental findRentalById(Long id) {
+        Optional<Rental> optionalRental = rentalRepository.findById(id);
+        if (optionalRental.isPresent()) {
+            Rental rental = optionalRental.get();
+            return rental;
+        } else {
+            throw new RuntimeException();
+        }
+    }
 }
