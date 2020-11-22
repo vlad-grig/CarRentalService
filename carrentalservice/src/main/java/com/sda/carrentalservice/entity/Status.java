0fbcf297f0c8822ef1c388670088ddc65c04d6f6
Vlad Grigore <vladyg1986@gmail.com>
2020-11-22 13:02:39 +0200

    no message

--- carrentalservice/src/main/java/com/sda/carrentalservice/entity/Status.java
+++ carrentalservice/src/main/java/com/sda/carrentalservice/entity/Status.java
@@ -1,5 +1,19 @@
 package com.sda.carrentalservice.entity;
 
 public enum Status {
-    NOT_AVAILABLE, BROKEN, IN_REPAIR, IN_SERVICE, AVAILABLE;
+    NOT_AVAILABLE("NOT AVAILABLE"),
+    BROKEN("BROKEN"),
+    IN_REPAIR("IN REPAIR"),
+    IN_SERVICE("IN SERVICE"),
+    AVAILABLE("AVAILABLE");
+
+    private final String displayName;
+
+    Status(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
 }
