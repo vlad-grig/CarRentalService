1212316a8b1776c8b4dd022e4cbb9d967f949a14
George Niculae <george.niculae20@yahoo.com>
2020-11-22 10:46:47 +0200

    no message

--- carrentalservice/src/main/java/com/sda/carrentalservice/mvccontroller/RentalOfficeMVCController.java
+++ carrentalservice/src/main/java/com/sda/carrentalservice/mvccontroller/RentalOfficeMVCController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
-import java.util.List;
 
 @Controller
 public class RentalOfficeMVCController {
