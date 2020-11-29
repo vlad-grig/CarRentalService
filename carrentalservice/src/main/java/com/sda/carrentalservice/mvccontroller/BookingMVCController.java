d53d82fb1299027847a10e9be31778865b2331d1
George Niculae <george.niculae20@yahoo.com>
2020-11-29 18:11:37 +0200

    no message

--- carrentalservice/src/main/java/com/sda/carrentalservice/mvccontroller/BookingMVCController.java
+++ carrentalservice/src/main/java/com/sda/carrentalservice/mvccontroller/BookingMVCController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
-
 import javax.validation.Valid;
 
 @Controller
