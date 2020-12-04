6d32e3401abde2a2966f88c8d5bb7175f1310b1f
Vlad Grigore <vladyg1986@gmail.com>
2020-12-04 20:45:03 +0200

    returnCarService

--- carrentalservice/src/main/java/com/sda/carrentalservice/service/ReturnCarService.java
+++ carrentalservice/src/main/java/com/sda/carrentalservice/service/ReturnCarService.java
@@ -1,8 +1,13 @@
 package com.sda.carrentalservice.service;
 
+import com.sda.carrentalservice.entity.ReturnCar;
+import com.sda.carrentalservice.exception.NotFoundException;
 import com.sda.carrentalservice.repository.ReturnCarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ReturnCarService {
@@ -12,5 +17,30 @@ public class ReturnCarService {
     @Autowired
     public ReturnCarService(ReturnCarRepository returnCarRepository) {
         this.returnCarRepository = returnCarRepository;
+    }
+
+    public ReturnCar saveReturnCar(ReturnCar returnCar) {
+        return returnCarRepository.save(returnCar);
+    }
+
+    public List<ReturnCar> findAllReturnCar() {
+        return returnCarRepository.findAll();
+    }
+
+    public ReturnCar findReturnCarById(Long id) {
+        Optional<ReturnCar> optionalReturnCar = returnCarRepository.findById(id);
+        if (optionalReturnCar.isPresent()) {
+            return optionalReturnCar.get();
+        } else
+            throw new NotFoundException("Return car with id " + id + "does not exist.");
+    }
+
+    public void deleteReturnCarById(Long id) {
+        this.findReturnCarById(id);
+        returnCarRepository.deleteById(id);
+    }
+
+    public Long countReturnCar() {
+        return returnCarRepository.count();
     }
 }
