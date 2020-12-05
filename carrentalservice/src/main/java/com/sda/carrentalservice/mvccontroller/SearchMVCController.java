6d8606bb71e737f0a25658248cb73f1e03644740
George Niculae <george.niculae20@yahoo.com>
2020-12-05 13:41:33 +0200

    no message

--- carrentalservice/src/main/java/com/sda/carrentalservice/mvccontroller/SearchMVCController.java
+++ carrentalservice/src/main/java/com/sda/carrentalservice/mvccontroller/SearchMVCController.java
@@ -1,2 +1,24 @@
-package com.sda.carrentalservice.mvccontroller;public class SearchMVCController {
+package com.sda.carrentalservice.mvccontroller;
+
+import com.sda.carrentalservice.service.CarService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class SearchMVCController {
+
+    private final CarService carService;
+
+    @Autowired
+    public SearchMVCController(CarService carService) {
+        this.carService = carService;
+    }
+
+    @PostMapping(path = "/")
+    public String search(String searchString, Model model) {
+        model.addAttribute("cars", this.carService.findCarByName(searchString));
+        return "car-list";
+    }
 }
