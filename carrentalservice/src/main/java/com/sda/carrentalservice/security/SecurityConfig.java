9d0cb74b7d5c55eec5e8168809085e6ffafb9155
George Niculae <george.niculae20@yahoo.com>
2020-12-05 11:02:49 +0200

    no message

--- carrentalservice/src/main/java/com/sda/carrentalservice/security/SecurityConfig.java
+++ carrentalservice/src/main/java/com/sda/carrentalservice/security/SecurityConfig.java
@@ -19,6 +19,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
+        userService.createUsers();
         //define which endpoints/resources(css) can be accessed by anyone
         http.authorizeRequests().antMatchers("/register", "/user/register").permitAll()
                 .antMatchers("/branch/**", "/car/**", "/customer/**", "/employee/**", "/rentaloffice/**").hasRole("ADMIN")
