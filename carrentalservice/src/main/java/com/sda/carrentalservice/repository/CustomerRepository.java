2d5d0426846eb68e6f23c3ac3ef5c9b51189f8ab
George Niculae <george.niculae20@yahoo.com>
2020-12-05 11:13:51 +0200

    no message

--- carrentalservice/src/main/java/com/sda/carrentalservice/repository/CustomerRepository.java
+++ carrentalservice/src/main/java/com/sda/carrentalservice/repository/CustomerRepository.java
@@ -4,6 +4,10 @@ import com.sda.carrentalservice.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    Optional<Customer> findCustomerByUsername(String username);
 }
