5782288249f8d0e3765e1cde4f41b41b230ff139
George Niculae <george.niculae20@yahoo.com>
2020-12-06 12:07:40 +0200

    no message

--- carrentalservice/src/main/java/com/sda/carrentalservice/repository/BranchRepository.java
+++ carrentalservice/src/main/java/com/sda/carrentalservice/repository/BranchRepository.java
@@ -2,8 +2,13 @@ package com.sda.carrentalservice.repository;
 
 import com.sda.carrentalservice.entity.Branch;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BranchRepository extends JpaRepository<Branch, Long> {
+
+    @Query("From Branch branch where lower(branch.name) like '%:branchName%' or lower(branch.rentalOffice) like '%:branchName%'")
+    Branch findBranchByName(@Param("branchName") String branchName);
 }
--- carrentalservice/src/main/java/com/sda/carrentalservice/repository/EmployeeRepository.java
+++ carrentalservice/src/main/java/com/sda/carrentalservice/repository/EmployeeRepository.java
@@ -2,8 +2,13 @@ package com.sda.carrentalservice.repository;
 
 import com.sda.carrentalservice.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    @Query("From Employee employee where lower(employee.firstName) like '%:employeeName%' or lower(employee.lastName) like '%:employeeName%'")
+    Employee findEmployeeByName(@Param("employeeName") String employeeName);
 }
